67bcd560f61b939bcd41d8922ee0277af7a51103
twith2e <abdulbasitadebajo1619@gmail.com>
2024-07-06 22:33:08 +0100

    major update

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
-function updateList() {
-  const listInput = document.getElementById("to-do-input");
-  const list = document.getElementById("todo-list");
+const listInput = document.getElementById("todoInput");
+const editInput = document.getElementById("editInput");
+const listOutput = document.getElementById("todo-list");
+let globalIndex = "";
+let taskDatabase = JSON.parse(localStorage.getItem("list")) || [];
 
-  if (listInput.value === "") {
-    alert("Please enter a task!");
-    return;
+taskDatabase.forEach((list, index) => {
+  listOutput.innerHTML += `<li class='task'>${list}<div class="action-btn"><button onclick="editTask(${index})" class="edit-btn" value="">
+  <span class="material-symbols-outlined" id="edit-icon">edit</span></button><button class="delete-btn" value="" onclick="deleteTask(${index})">
+  <span class="material-symbols-outlined" id="delete-icon">delete</span></button></div></li>`;
+});
+
+function addList() {
+  if (listInput.value) {
+    taskDatabase.push(listInput.value);
+    localStorage.setItem("list", JSON.stringify(taskDatabase));
+    listOutput.innerHTML = "";
+    taskDatabase.forEach((list, index) => {
+      listOutput.innerHTML += `<li class="task">${list}<div class="action-btn"><button class="edit-btn" onclick="editTask(${index})" value="">
+      <span class="material-symbols-outlined" id="edit-icon">edit</span></button><button class="delete-btn" value="" onclick="deleteTask(${index})">
+      <span class="material-symbols-outlined" id="delete-icon">delete</span></button></div></li>`;
+    });
+  } else {
+    alert("fill input");
   }
-
-  li = document.createElement("li");
-  li.textContent = listInput.value;
-  list.appendChild(li);
-  buttonsDiv = document.createElement("div");
-  buttonsDiv.className = "buttons";
-  list.appendChild(buttonsDiv);
-
-  // Clear the input field
   listInput.value = "";
-
-  editButton = document.createElement("button");
-  editButton.textContent = "Edit";
-  editButton.className = "edit-btn";
-  editButton.onclick = function () {
-    editTask(li);
-  };
-  buttonsDiv.appendChild(editButton);
-  deleteButton = document.createElement("button");
-  deleteButton.textContent = "X";
-  deleteButton.className = "delete-btn";
-  deleteButton.onclick = function () {
-    deleteTask(list);
-  };
-  buttonsDiv.appendChild(deleteButton);
-
-  list.appendChild(buttonsDiv);
-  list.appendChild(li);
 }
 
-function editTask(taskSpan) {
-  newTask = prompt("Edit your task:", taskSpan.textContent);
-  if (newTask !== null && newTask !== "") {
-    taskSpan.textContent = newTask;
-  }
+function editTask(index) {
+  globalIndex = index;
+  thisInput.style.display = "none";
+  thisInput1.style.display = "flex";
+  editInput.value = taskDatabase[index];
 }
 
-function deleteTask(taskItem) {
-  taskItem.remove();
+function editList() {
+  taskDatabase.splice(globalIndex, 1, editInput.value);
+  localStorage.setItem("list", JSON.stringify(taskDatabase));
+  listOutput.innerHTML = "";
+  taskDatabase.forEach((list, index) => {
+    listOutput.innerHTML += `<li class="task">${list}<div class="action-btn"><button class="edit-btn" onclick="editTask(${index})" value="">
+      <span class="material-symbols-outlined" id="edit-icon">edit</span></button><button class="delete-btn" value="" onclick="deleteTask(${index})">
+      <span class="material-symbols-outlined" id="delete-icon">delete</span></button></div></li>`;
+  });
+  thisInput.style.display = "flex";
+  thisInput1.style.display = "none";
+}
+
+function deleteTask(index) {
+  taskDatabase.splice(index, 1);
+  localStorage.setItem("list", JSON.stringify(taskDatabase));
+  listOutput.innerHTML = "";
+  taskDatabase.forEach((list, index) => {
+    listOutput.innerHTML += `<li class="task">${list}<div class="action-btn"><button class="edit-btn" onclick="editTask(${index})" value="">
+      <span class="material-symbols-outlined" id="edit-icon">edit</span></button><button class="delete-btn" value="" onclick="deleteTask(${index})">
+      <span class="material-symbols-outlined" id="delete-icon">delete</span></button></div></li>`;
+  });
 }
